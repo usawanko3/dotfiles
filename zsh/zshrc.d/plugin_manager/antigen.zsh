ced1022609b69608e10eb8dad6b51e8de8092a04
usawanko3 <usawanko3@gmail.com>
2016-01-07 06:46:33 +0900

    Install and enable zsh-syntax-highlighting

--- zsh/zshrc.d/plugin_manager/antigen.zsh
+++ zsh/zshrc.d/plugin_manager/antigen.zsh
@@ -4,5 +4,8 @@ source ~/.zsh/antigen/antigen.zsh
 # Completion
 antigen bundle zsh-users/zsh-completions src
 
+# Syntax highlighting
+antigen bundle zsh-users/zsh-syntax-highlighting
+
 # Tell antigen that you're done.
 antigen apply
