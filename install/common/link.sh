7ba78e5a2b8d1045f34e9d5744bb6862cee97102
usawanko3 <usawanko3@gmail.com>
2016-01-07 05:29:05 +0900

    Source separated zshrcs on zsh

--- install/common/link.sh
+++ install/common/link.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 ln -nfs ~/dotfiles/atom ~/.atom
+
+ln -fs ~/dotfiles/zshrc ~/.zshrc
+ln -nfs ~/dotfiles/zsh ~/.zsh
---
+++ zshrc
@@ -0,0 +1,6 @@
+#
+# Source separated zshrcs
+#
+for file in ~/.zsh/zshrc.d/zsh/*.zsh; do
+  source $file
+done
