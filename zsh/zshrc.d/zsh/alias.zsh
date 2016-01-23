e621adab1397bf083f72e4e549bc833b19bcfb68
usawanko3 <usawanko3@gmail.com>
2016-01-23 09:53:50 +0000

    Add alias for gvim on linux

--- zsh/zshrc.d/zsh/alias.zsh
+++ zsh/zshrc.d/zsh/alias.zsh
@@ -1,6 +1,12 @@
 # Editor
 alias v='vim'
-alias gv='/Applications/MacVim.app/Contents/MacOS/gvim'
+
+sys_name=`uname`
+if [ $sys_name = 'Darwin' ]; then
+  alias gv='/Applications/MacVim.app/Contents/MacOS/gvim'
+elif [ $sys_name = 'Linux' ]; then
+  alias gv='gvim'
+fi
 
 # Grep
 alias -g P='| peco'
